3bff65b7c455d44feb57ca523f268b6dd3c375a8
arnaulloveras <arnau.lloveras@institutvidreres.cat>
2022-12-14 09:07:57 +0100

    5é Commit tot passat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import {RegistreComponent} from "./registre/registre.component";
-import { AppRoutingModule } from './app-routing.module';
+import { IndexComponent } from './index/index.component';
+import { FooterComponent } from './footer/footer.component';
+import { CatalegComponent } from './cataleg/cataleg.component';
+import {CistellaComponent} from "./cistella/cistella.component";
 import {ContacteComponent} from "./contacte/contacte.component";
+import {RegistreComponent} from "./registre/registre.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RegistreComponent,
-    ContacteComponent
+    IndexComponent,
+    FooterComponent,
+    CatalegComponent,
+    CistellaComponent,
+    ContacteComponent,
+    RegistreComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
