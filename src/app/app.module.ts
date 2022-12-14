c06a95f724f8fb17a994dc1c1869b061d8a694ff
arnaulloveras <arnau.lloveras@institutvidreres.cat>
2022-12-14 09:46:28 +0100

    7é Commit ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import {CondicionsComponent} from "./condicions/condicions.component";
     ContacteComponent,
     RegistreComponent
   ],
+  //import
   imports: [
     BrowserModule,
     RouterModule.forRoot([
