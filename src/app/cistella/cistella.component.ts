30c1a89d6cfdce021ad8cf26e9ad2fc4121188ab
arnaulloveras <arnau.lloveras@institutvidreres.cat>
2022-12-19 09:21:03 +0100

    10é Commit

--- src/app/cistella/cistella.component.ts
+++ src/app/cistella/cistella.component.ts
@@ -6,7 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./cistella.component.css']
 })
 export class CistellaComponent {
+
+  total:any;
+
+  constructor() {
+    this.total = 0;
+  }
+
   ngOnInit(){
+    let preu1 = document.getElementById("preu_1");
+    let preu2 = document.getElementById("preu_2");
+    let preu3 = document.getElementById("preu_3");
+    let preu4 = document.getElementById("preu_4");
+    let preu5 = document.getElementById("preu_5");
+    let preu6 = document.getElementById("preu_6");
+
     let suma = document.getElementById("sumar");
     let resta = document.getElementById("restar");
 
@@ -14,6 +28,7 @@ export class CistellaComponent {
 
     let num_prod = 0;
 
+    let ola = 0;
 
 
     //@ts-ignore
@@ -21,7 +36,11 @@ export class CistellaComponent {
       //@ts-ignore
       num_prod++;
       //@ts-ignore
+      ola = (parseInt(preu1.innerHTML) * num_prod)
+      //@ts-ignore
       contador.innerHTML = num_prod;
+
+
     }
 
     //@ts-ignore
@@ -42,7 +61,7 @@ export class CistellaComponent {
     let suma_1_2 = document.getElementById("sumar_1_2");
     let resta_1_2 = document.getElementById("restar_1_2");
 
-    let contador_1_2 = document.getElementById("prod_cant_1_2");
+    let contador2 = document.getElementById("prod_cant_1_2");
 
     let num_prod_1_2 = 0;
 
@@ -53,7 +72,7 @@ export class CistellaComponent {
       //@ts-ignore
       num_prod_1_2++;
       //@ts-ignore
-      contador_1_2.innerHTML = num_prod_1_2;
+      contador2.innerHTML = num_prod_1_2;
     }
 
     //@ts-ignore
@@ -64,17 +83,17 @@ export class CistellaComponent {
       if (num_prod_1_2 < 0){
         num_prod_1_2 = 0;
         //@ts-ignore
-        contador_1_2.innerHTML = num_prod_1_2;
+        contador2.innerHTML = num_prod_1_2;
       } else{
         //@ts-ignore
-        contador_1_2.innerHTML = num_prod_1_2;
+        contador2.innerHTML = num_prod_1_2;
       }
     }
 
     let suma_1_3 = document.getElementById("sumar_1_3");
     let resta_1_3 = document.getElementById("restar_1_3");
 
-    let contador_1_3 = document.getElementById("prod_cant_1_3");
+    let contador3 = document.getElementById("prod_cant_1_3");
 
     let num_prod_1_3 = 0;
 
@@ -85,7 +104,7 @@ export class CistellaComponent {
       //@ts-ignore
       num_prod_1_3++;
       //@ts-ignore
-      contador_1_3.innerHTML = num_prod_1_3;
+      contador3.innerHTML = num_prod_1_3;
     }
 
     //@ts-ignore
@@ -96,17 +115,17 @@ export class CistellaComponent {
       if (num_prod_1_3 < 0){
         num_prod_1_3 = 0;
         //@ts-ignore
-        contador_1_3.innerHTML = num_prod_1_3;
+        contador3.innerHTML = num_prod_1_3;
       } else{
         //@ts-ignore
-        contador_1_3.innerHTML = num_prod_1_3;
+        contador3.innerHTML = num_prod_1_3;
       }
     }
 
     let suma2 = document.getElementById("sumar2");
     let resta2 = document.getElementById("restar2");
 
-    let contador2 = document.getElementById("prod_cant2");
+    let contador4 = document.getElementById("prod_cant2");
 
     let num_prod2 = 0;
 
@@ -115,7 +134,7 @@ export class CistellaComponent {
       //@ts-ignore
       num_prod2++;
       //@ts-ignore
-      contador2.innerHTML = num_prod2;
+      contador4.innerHTML = num_prod2;
     }
 
     //@ts-ignore
@@ -126,11 +145,118 @@ export class CistellaComponent {
       if (num_prod2 < 0){
         num_prod2 = 0;
         //@ts-ignore
-        contador2.innerHTML = num_prod2;
+        contador4.innerHTML = num_prod2;
       } else{
         //@ts-ignore
-        contador2.innerHTML = num_prod2;
+        contador4.innerHTML = num_prod2;
       }
+    }
+
+    let suma2_2 = document.getElementById("sumar_2_2");
+    let resta2_2 = document.getElementById("restar_2_2");
+
+    let contador5 = document.getElementById("prod2_cant2");
+
+    let num_prod2_2 = 0;
+
+    //@ts-ignore
+    suma2_2.onclick = function sumar(){
+      //@ts-ignore
+      num_prod2_2++;
+      //@ts-ignore
+      contador5.innerHTML = num_prod2_2;
+    }
+
+    //@ts-ignore
+    resta2_2.onclick = function restar(){
+      //@ts-ignore
+      num_prod2_2--;
+      //@ts-ignore
+      if (num_prod2_2 < 0){
+        num_prod2_2 = 0;
+        //@ts-ignore
+        contador5.innerHTML = num_prod2_2;
+      } else{
+        //@ts-ignore
+        contador5.innerHTML = num_prod2_2;
+      }
+    }
+
+    let suma2_3 = document.getElementById("sumar_2_3");
+    let resta2_3 = document.getElementById("restar_2_3");
+
+    let contador6 = document.getElementById("prod2_cant3");
+
+    let num_prod2_3 = 0;
+
+    //@ts-ignore
+    suma2_3.onclick = function sumar(){
+      //@ts-ignore
+      num_prod2_3++;
+      //@ts-ignore
+      contador6.innerHTML = num_prod2_3;
+    }
+
+    //@ts-ignore
+    resta2_3.onclick = function restar(){
+      //@ts-ignore
+      num_prod2_3--;
+      //@ts-ignore
+      if (num_prod2_3 < 0){
+        num_prod2_3 = 0;
+        //@ts-ignore
+        contador6.innerHTML = num_prod2_3;
+      } else{
+        //@ts-ignore
+        contador6.innerHTML = num_prod2_3;
+      }
+    }
+
+    // @ts-ignore
+    this.total = parseInt(preu1.innerHTML)* num_prod + '€';
+
+
+    const el = document.getElementById('contenidor1')
+
+    const buto = document.getElementById('buto_bulls')
+
+    if (el != null && buto != null){
+      buto.addEventListener('click', function Click(){
+        if (el.style.display === 'none') {
+          el.style.display = 'block';
+
+          buto.textContent = 'Bulls'
+
+          buto.style.backgroundColor = 'grey'
+        } else{
+          el.style.display = 'none'
+
+          buto.textContent = 'Bulls'
+
+          buto.style.backgroundColor = 'red'
+        }
+      })
+    }
+
+    const el2 = document.getElementById('contenidor2')
+    const buto2 = document.getElementById('buto_bucks')
+
+    if (el2 != null && buto2 != null){
+      buto2.addEventListener('click', function Click(){
+        if (el2.style.display === 'none') {
+          el2.style.display = 'block';
+
+          buto2.textContent = 'Bucks'
+
+          buto2.style.backgroundColor = 'grey'
+        } else{
+          el2.style.display = 'none'
+
+          buto2.textContent = 'Bucks'
+
+          buto2.style.backgroundColor = 'red'
+        }
+      })
     }
 
   }
