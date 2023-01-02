04668e48979f356a0f13d66282ff3f6fc25cadf3
arnaulloveras <arnau.lloveras@institutvidreres.cat>
2023-01-02 19:23:38 +0100

    Commit amb els noms, i preu automàtic al catàleg

--- src/app/cataleg/cataleg.component.ts
+++ src/app/cataleg/cataleg.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-cataleg',
   templateUrl: './cataleg.component.html',
   styleUrls: ['./cataleg.component.css']
 })
 export class CatalegComponent {
+
+  //@ts-ignore
+  productes: producte[] =
+    [{
+    nom: "Samarreta Bucks", preu: 79.99,
+    quantitat: 0,
+    imatge: "assets/samarretabucks.png"},
+      {nom: "Gorra Bucks",
+      preu: 32.09,
+      quantitat: 0,
+      image: "assets/bufanda_buks.jpg"},
+      {nom: "Pijama Bucks",
+      preu: 24,
+      quantitat: 0,
+      imatge: "assets/pijama_bucks.png"}]
+  //@ts-ignore
+  productes2: producte2[] =
+    [{
+      nom: "Samarreta Bulls", preu: 79.99,
+      quantitat: 0,
+      imatge: "assets/samarretabulls.png"},
+      {nom: "Calçotets",
+        preu: 14.99,
+        quantitat: 0,
+        image: "assets/calzoncillos_bulls.jpeg"},
+      {nom: "Mascota Bulls",
+        preu: 11.99,
+        quantitat: 0,
+        imatge: "assets/mascota_bulls.jpeg"}]
   ngOnInit(){
     const el = document.getElementById('contenidor1')
 
