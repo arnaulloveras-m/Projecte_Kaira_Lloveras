06c02a9901f5c1d06718e44ccc26128247cda730
arnaulloveras-m <arnaulloverasmorales@gmail.com>
2023-01-08 01:10:00 +0100

    Commit LocalStorage Carrito

--- src/app/cataleg/cataleg.component.ts
+++ src/app/cataleg/cataleg.component.ts
@@ -7,35 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./cataleg.component.css']
 })
 export class CatalegComponent {
+constrcutor(){
+
+  this.grabar_localstorage();
+
+}
 
   //@ts-ignore
-  productes: producte[] =
-    [{
+  grabar_localstorage(){
+  let samarretabucks =
+    {
     nom: "Samarreta Bucks", preu: 79.99,
     quantitat: 0,
-    imatge: "assets/samarretabucks.png"},
+    imatge: "assets/samarretabucks.png"}
+    let bufandabucks =
       {nom: "Gorra Bucks",
       preu: 32.09,
       quantitat: 0,
-      image: "assets/bufanda_buks.jpg"},
+      image: "assets/bufanda_buks.jpg"}
+      let pijamabucks =
       {nom: "Pijama Bucks",
       preu: 24,
       quantitat: 0,
-      imatge: "assets/pijama_bucks.png"}]
+      imatge: "assets/pijama_bucks.png"}
   //@ts-ignore
-  productes2: producte2[] =
-    [{
+  let samarretabulls =
+    {
       nom: "Samarreta Bulls", preu: 79.99,
       quantitat: 0,
-      imatge: "assets/samarretabulls.png"},
+      imatge: "assets/samarretabulls.png"}
+      let calcotetsbulls =
       {nom: "Calçotets",
         preu: 14.99,
         quantitat: 0,
-        image: "assets/calzoncillos_bulls.jpeg"},
+        image: "assets/calzoncillos_bulls.jpeg"}
+        let mascotabulls =
       {nom: "Mascota Bulls",
         preu: 11.99,
         quantitat: 0,
-        imatge: "assets/mascota_bulls.jpeg"}]
+        imatge: "assets/mascota_bulls.jpeg"}
+        localStorage.setItem("Mascota Bulls", JSON.stringify(mascotabulls))
+        }
+
   ngOnInit(){
     const el = document.getElementById('contenidor1')
 
